23b9835491ff73bfbea948b90c8c0dbbfacb2c5b
Cristian <tejedor92@gmail.com>
2021-04-15 15:47:57 +0200

    done

--- docker/schema.sql
+++ docker/schema.sql
@@ -1,3 +1,9 @@
+CREATE TABLE test_table(
+                     id serial PRIMARY KEY,
+                     title VARCHAR(255) UNIQUE NOT NULL,
+                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                     ); 
+
 CREATE TABLE test_table2(
                      id serial PRIMARY KEY,
                      num  SMALLINT NOT NULL,
@@ -7,10 +13,4 @@ CREATE TABLE test_table2(
                      info json,
                      bobo boolean 
                     ); 
-
-CREATE TABLE test_table(
-                     id serial PRIMARY KEY,
-                     title VARCHAR(255) UNIQUE NOT NULL,
-                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                     ); 
 
